c7f7dce704aabfa8f8c621d87ceb6c925f758739
Rodrigo Bussolo <rbussolo91@gmail.com>
2021-03-17 17:45:09 -0400

    Feito basicamente todo o layout.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --red: #E52E4D;
         --blue: #5429CC;
-
+        --green: #33CC95;
         --blue-light: #6933FF;
 
         --text-title: #363F5F;
@@ -51,5 +51,42 @@ export const GlobalStyle = createGlobalStyle`
     [disabled] {
         opacity: 0.6;
         cursor: not-allowed;
+    }
+
+    .react-modal-overlay{
+        background: rgba(0, 0, 0, 0.5);
+
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .react-modal-content{
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position:relative;
+        border-radius: 0.25rem;
+    }
+
+    .react-modal-close{
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+
+        transition: filter 0.2s;
+
+        &:hover{
+            filter: brightness(0.8);
+        }
     }
 `;
